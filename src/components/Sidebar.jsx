a2c607d84bbde5e13fdb9d9469131a7a76c1ce1d
Sonu Kumar <sonu085.hitit2020@gmail.com>
2025-05-27 03:26:48 +0530

    new build file

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ export default function Sidebar() {
   return (
     <div className="w-64 bg-white shadow-lg px-6 py-8 flex flex-col justify-between">
       <div>
-        <h1 className="text-3xl font-bold text-blue-600 mb-12">Healthg<span className="text-black">care.</span></h1>
+        <h1 className="text-3xl font-bold text-blue-600 mb-12">Health<span className="text-black">care.</span></h1>
         <nav className="space-y-8">
           <div>
             <h2 className="text-sm text-gray-400 uppercase mb-2">General</h2>
